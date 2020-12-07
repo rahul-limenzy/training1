64e2175dfe4ce339acdbb980f24eb61c74cf4082
rahul-limenzy <rahul.limenzy@gmail.com>
2020-12-07 11:27:59 +0530

    front page

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('login');
-});
-Route::get('/register', function () {
-    return view('register');
-});
-
-Route::get('/homepage', function () {
-    return view('homepage');
+    return view('welcome');
 });
